49c20f7b585db38ba530b0f3156e81213d521ea2
how2vm <37172143+how2vm@users.noreply.github.com>
2018-09-20 15:25:50 +0200

    Update VMBulk_create.ps1

--- VMBulk_create.ps1
+++ VMBulk_create.ps1
@@ -4,12 +4,17 @@ For ($i=105; $i -le 110; $i++) {
     $VMCPU = Get-Random -InputObject 2, 4, 8
     $IP = $VMname
     $GW = "192.168.4.1"
+    $NM = "255.255.255.0"
+    $Host = "testhost.lab.local"
+    $Datastore = "vsanDatastore"
+    $Customization = "TestVM_profile"
+    $SourceVM = "SourceVMName"
     write-host "Name is  $VMname"
-    new-vm -Name $VMname -VM 192.168.4.104 -VMhost lon06vsanstretchedcluster03.sdslab.net -Datastore vsanStretchedDatastore -OSCustomizationSpec TestVM_lon04
+    new-vm -Name $VMname -VM $SourceVM -VMhost $Host -Datastore $Datastore -OSCustomizationSpec $Customization
     Start-Sleep -Seconds 20
     Set-VM -VM $VMname -MemoryGB $VMMemory -NumCPU $VMCPU –Confirm:$False
     Start-Sleep -Seconds 10
     start-VM -VM $VMname
     Start-Sleep -Seconds 150
-    Invoke-VMScript -VM $VMname "netsh interface ipv4 set addres ""Ethernet0"" static $IP 255.255.255.0 $GW" -GuestUser administrator -GuestPassword !QAZ2wsx
+    Invoke-VMScript -VM $VMname "netsh interface ipv4 set addres ""Ethernet0"" static $IP $NM $GW" -GuestUser administrator -GuestPassword !QAZ2wsx
     }
