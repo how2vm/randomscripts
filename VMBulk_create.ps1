4e52580371ae0232283c9d0777220f10a6d411d0
Aram Avetisyan <czz45147@Arams-MacBook-Pro.local>
2019-04-18 10:01:33 +0200

    Added a loop to wait for VMware Tools status

--- VMBulk_create.ps1
+++ VMBulk_create.ps1
@@ -5,16 +5,23 @@ For ($i=105; $i -le 110; $i++) {
     $IP = $VMname
     $GW = "192.168.4.1"
     $NM = "255.255.255.0"
-    $Host = "testhost.lab.local"
+    $ESXiHost = "testhost.lab.local"
     $Datastore = "vsanDatastore"
     $Customization = "TestVM_profile"
     $SourceVM = "SourceVMName"
     write-host "Name is  $VMname"
-    new-vm -Name $VMname -VM $SourceVM -VMhost $Host -Datastore $Datastore -OSCustomizationSpec $Customization
+    new-vm -Name $VMname -VM $SourceVM -VMhost $ESXiHost -Datastore $Datastore -OSCustomizationSpec $Customization
     Start-Sleep -Seconds 20
     Set-VM -VM $VMname -MemoryGB $VMMemory -NumCPU $VMCPU –Confirm:$False
     Start-Sleep -Seconds 10
     start-VM -VM $VMname
-    Start-Sleep -Seconds 150
-    Invoke-VMScript -VM $VMname "netsh interface ipv4 set addres ""Ethernet0"" static $IP $NM $GW" -GuestUser administrator -GuestPassword !QAZ2wsx
-    }
+    write-host “Waiting for VM Tools to Start”
+    write-host “Waiting for VM Tools to Start”
+    do {
+    $toolsStatus = (Get-VM $VMname | Get-View).Guest.ToolsStatus
+    write-host $toolsStatus
+    sleep 3
+    } until ( $toolsStatus -eq ‘toolsOk’ )
+    Invoke-VMScript -VM $VMname "netsh interface ipv4 set address Ethernet0 static $IP $NM $GW" -GuestUser administrator -GuestPassword !QAZ2wsx
+    Invoke-VMScript -VM $VMname "netsh interface ipv4 show config" -GuestUser administrator -GuestPassword !QAZ2wsx
+}
